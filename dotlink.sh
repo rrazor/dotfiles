46a8c114a4bd1b1bd50a326a59d613da679d2c76
Matt Gray <rrazor@gmail.com>
2012-03-17 23:42:17 -0500

    Do not overwrite existing .bak files

--- dotlink.sh
+++ dotlink.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -eu
 
-SRC=$(readlink -f $1)
-DEST="$HOME/$1"
+REL=${1}
+SRC=$(readlink -f $REL)
+DEST="$HOME/$(basename $REL)"
+BAK="$DEST.bak"
 
-if [ -e $DEST ]; then
-	if [ -L $DEST ]; then
-		rm $DEST
-	else
-		# Back up the file
-		BACKUP="$DEST.bak"
-		mv $DEST $BACKUP
-		echo "    BACKUP: $DEST => $BACKUP"
+if [ -L $DEST ]; then
+	rm $DEST
+	echo "    UNLINK: $DEST"
+elif [ -e $DEST ]; then
+	if [ -e $BAK ]; then
+		echo "ERROR: Will not overwrite backup: $BAK"
+		exit 1
 	fi
+	mv $DEST $BAK
+	echo "    BACKUP: $DEST => $BAK"
 fi
 
 ln -s $SRC $DEST
-echo "    LINK  : $DEST => $SRC"
+echo "    LINK  : $SRC => $DEST"
