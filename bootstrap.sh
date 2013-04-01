97f68902d1b7627198e7db055437573bccc325ca
Matt Gray <rrazor@gmail.com>
2013-03-31 21:23:53 -0500

    Ignore the .gitmodules as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".gitmodules" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
