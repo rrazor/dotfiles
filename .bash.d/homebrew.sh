880546b48697985c4d6720417ae0f11845334705
Matt Gray <rrazor@gmail.com>
2017-03-28 07:50:46 -0500

    Comment out Homebrew nvm

--- .bash.d/homebrew.sh
+++ .bash.d/homebrew.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Homebrew-specific entries in here
-if [ `uname -s` == "Darwin" ]; then
-	# Brew-installed nvm, so we can use npm outside of homebrew
-	source $(brew --prefix nvm)/nvm.sh
-fi
+export PATH=${PATH}:${HOME}/homebrew/bin
+#if [ `uname -s` == "Darwin" ]; then
+#	# Brew-installed nvm, so we can use npm outside of homebrew
+#	# source $(brew --prefix nvm)/nvm.sh
+#fi
