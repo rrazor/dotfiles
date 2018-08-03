fe2a7d72541d462ff962dfb77fa1ca49c6c882d5
Matt Gray <rrazor@gmail.com>
2018-08-03 11:41:32 -0500

    Undo employer-specific local homebrew/

--- .bash.d/homebrew.sh
+++ .bash.d/homebrew.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 # Homebrew-specific entries in here
-export PATH=${PATH}:${HOME}/homebrew/bin
-#if [ `uname -s` == "Darwin" ]; then
-#	# Brew-installed nvm, so we can use npm outside of homebrew
-#	# source $(brew --prefix nvm)/nvm.sh
-#fi
